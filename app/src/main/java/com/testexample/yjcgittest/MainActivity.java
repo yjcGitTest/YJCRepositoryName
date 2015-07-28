2dff179ac19b81d52d43c40bf9d5dd09ed711df2
kimsaeyoung <dsflfkew7@naver.com>
2015-07-28 13:21:52 +0900

    변수를 만들었습니다.

--- app/src/main/java/com/testexample/yjcgittest/MainActivity.java
+++ app/src/main/java/com/testexample/yjcgittest/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.MenuItem;
 
 
 public class MainActivity extends Activity {
-
+    int TestTJC;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
